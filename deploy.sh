18c4e86d5f1b4d0fdfea9a53a58913bbdab460b3
Jeff Wintersinger <jeff.git@wintersinger.com>
2014-08-28 13:05:13 -0600

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-git pull && git push && git checkout gh-pages && git merge master && git push && git checkout master
+git pull && \
+  git push && \
+  git checkout gh-pages && \
+  git pull && \
+  git merge master && \
+  git push && \
+  git checkout master
